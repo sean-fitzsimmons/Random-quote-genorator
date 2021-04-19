d63a88d38f520c6a315fdc5192e6eb37f4bfadcb
Sean Fitzsimmons <seanfitzsimmons12@gmail.com>
2021-04-19 13:24:00 -0700

    Complete project

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const quotes = [ "Never stop", "Be better", "Do not give up", "Keep drinking", "Call me an Uber", "Want to get some TacoBell?"]
-const p1Button = document.querySelector('#p1Button')
+const quotes = ['"Never stop" - Green lights', '"Be better" - My Girlfiend', '"Just do it" - Nike', '"Keep drinking" - My brain"', '"Call me an Uber" - My Liver', '"Want to get some TacoBell?" - My Stomach']
+const p1Button = document.querySelector('#new-quote')
 const resetButton = document.querySelector('#reset')
-const displayQuote = document.querySelector('#newQuote')
+
  
 p1Button.addEventListener('click', function () {
 let randomNumber = Math.floor(Math.random() * quotes.length);
-document.getElementById('newQuote').innerHTML = quotes[randomNumber];
+document.getElementById('text').innerHTML = quotes[randomNumber];
 })
 
 resetButton.addEventListener('click', function () {
-    document.getElementById('newQuote').innerHTML = "Find your quote here!"; 
+    document.getElementById('text').innerHTML = "Find your quote here!"; 
     })
